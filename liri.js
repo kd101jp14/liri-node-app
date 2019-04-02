330fa0da934e4f0b1fe158026427354f1b1e9f0b
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-01 21:10:45 -0500

    Created code to concatenate input for for URL in axios API calls.

--- liri.js
+++ liri.js
@@ -4,3 +4,22 @@
 
 // var spotify = new Spotify(keys.spotify);
 
+// var command = process.argv[2];
+
+
+// var commandString = {
+//     concert: "concert-this",
+//     song: "spotify-this-song",
+//     movie: "movie-this",
+//     whatItSays: "do-what-it-says"
+// }
+
+var nodeArg = process.argv;
+var newArray = [];
+var formattedInput = "";
+for (var i = 2; i < nodeArg.length; i++) {
+    newArray.push(nodeArg[i]);
+    formattedInput = newArray.join("+");
+}
+console.log(newArray);
+console.log(formattedInput);
