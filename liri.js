e1ea919e21a32b04ec535e1e40e5ef124b29d09b
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-01 23:53:11 -0500

    Created switch statements for concerts, movies, and default.

--- liri.js
+++ liri.js
@@ -1,25 +1,60 @@
 // require("dotenv").config();
 
-// var keys = require("./keys.js");
+var keys = require("./keys.js");
 
 // var spotify = new Spotify(keys.spotify);
 
-// var command = process.argv[2];
+var command = process.argv[2];
 
 
-// var commandString = {
-//     concert: "concert-this",
-//     song: "spotify-this-song",
-//     movie: "movie-this",
-//     whatItSays: "do-what-it-says"
-// }
+var commandString = {
+    concert: "concert-this",
+    song: "spotify-this-song",
+    movie: "movie-this",
+    whatItSays: "do-what-it-says"
+}
 
 var nodeArg = process.argv;
 var newArray = [];
 var formattedInput = "";
-for (var i = 2; i < nodeArg.length; i++) {
+for (var i = 3; i < nodeArg.length; i++) {
     newArray.push(nodeArg[i]);
     formattedInput = newArray.join("+");
 }
+console.log(command);
 console.log(newArray);
 console.log(formattedInput);
+
+var axios = require("axios");
+
+switch (command) {
+    case commandString.concert:
+    var artist = formattedInput;
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    case commandString.movie:
+    var movie = formattedInput; 
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + movie)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    default: {
+        var movie = formattedInput; 
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + "mr+nobody")
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    }
+    
+}
