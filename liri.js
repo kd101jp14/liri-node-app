25a99fed5dda05eb71c2994784005cffa6eaa7ed
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-02 05:46:33 -0500

    Formatted responses for concerts and movies.

--- liri.js
+++ liri.js
@@ -26,35 +26,79 @@ console.log(newArray);
 console.log(formattedInput);
 
 var axios = require("axios");
+var moment = require("moment");
 
 switch (command) {
     case commandString.concert:
-    var artist = formattedInput;
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+        var artist = formattedInput;
+        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+            .then(function (response) {
+                console.log("Here are the next concert locations and dates for " + artist + "!");
+                console.log("\n");
+                for (var i = 0; i < response.data.length; i++) {
+                    var date = response.data[i].datetime;
+                    var format = "L";
+                    var formattedDate = moment(date).format(format);
+                    console.log(formattedDate + ": " + response.data[i].venue.name + " in " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+                }
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        break;
     case commandString.movie:
-    var movie = formattedInput; 
-    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + movie)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    default: {
-        var movie = formattedInput; 
-    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + "mr+nobody")
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    }
-    
+        var movie = formattedInput;
+        axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + movie)
+            .then(function (response) {
+
+                console.log("The movie " + response.data.Title + " was released in " + response.data.Year + ".");
+                console.log("\n");
+                console.log("Its IMDB rating is " + response.data.Ratings[0].Value + ", while its Rotten Tomatoes rating is " + response.data.Ratings[1].Value + ".");
+                console.log("\n");
+                console.log("This movie was produced in " + response.data.Country + ".");
+                console.log("\n");
+                console.log("Movie Language(s): " + response.data.Language + ".");
+                console.log("\n");
+                console.log("Plot: " + response.data.Plot);
+                console.log("\n");
+                console.log("This movie's actors include " + response.data.Actors + ".");
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        break;
+    case commandString.song:
+        var song = formattedInput;
+        axios.get("https://api.spotify.com/v1q=" + song + ":abacab&type=track")
+            .then(function (response) {
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        break;
+    case commandString.whatItSays:
+        var fs = require("fs");
+        fs.readFile("random.txt", "utf8", function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(data);
+            var dataArr = data.split(",");
+            console.log(dataArr);
+        })
+        break;
+    default:
+        {
+            var movie = formattedInput;
+            axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + "mr+nobody")
+            .then(function (response) {
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            break;
+        };
 }
